bec15e38ae71b5399280f7a7229dd00066cc4b2c
IVWebDev <ivweb@mail.ru>
2021-01-10 19:58:18 +0300

    create media 767px and burger

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+//header burger
+
+let headerBurger = document.querySelector('.header-burger');
+let headerNav = document.querySelector('.header-nav');
+let back = document.querySelector('body');
+let headerLinks = document.querySelector('.header-links');
+
+headerBurger.onclick = function () {
+   headerBurger.classList.toggle('active');
+   headerNav.classList.toggle('active');
+   back.classList.toggle('lock');
+}
+headerLinks.onclick = function () {
+   back.classList.toggle('lock');
+   headerBurger.classList.toggle('active');
+   headerNav.classList.toggle('active');
+}
 // slider
 var swiper = new Swiper('.swiper-container', {
    pagination: {
